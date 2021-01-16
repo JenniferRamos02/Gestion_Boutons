e6a95e54d9d2bf3eb2d7558e662de2e1c0b7fa55
JenniferRamos02 <77451878+JenniferRamos02@users.noreply.github.com>
2021-01-16 14:13:53 +0100

    Add files via upload
    ces sont des fichiers nécessaires pour faire fonctionner le projet si jamais n'est pas possible  télécharger le dossier

---
+++ avalon_anemometre_0.vhd
@@ -0,0 +1,51 @@
+-- avalon_anemometre_0.vhd
+
+-- This file was auto-generated as part of a generation operation.
+-- If you edit it your changes will probably be lost.
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity avalon_anemometre_0 is
+	port (
+		clk        : in  std_logic                     := '0';             --          clock.clk
+		chipselect : in  std_logic                     := '0';             -- avalon_slave_0.chipselect
+		write_n    : in  std_logic                     := '0';             --               .write_n
+		writedata  : in  std_logic_vector(31 downto 0) := (others => '0'); --               .writedata
+		readdata   : out std_logic_vector(31 downto 0);                    --               .readdata
+		address    : in  std_logic_vector(1 downto 0)  := (others => '0'); --               .address
+		reset_n    : in  std_logic                     := '0';             --          reset.reset_n
+		FREQ_IN    : in  std_logic                     := '0'              --    conduit_end.export
+	);
+end entity avalon_anemometre_0;
+
+architecture rtl of avalon_anemometre_0 is
+	component avalon_anemometre is
+		port (
+			clk        : in  std_logic                     := 'X';             -- clk
+			chipselect : in  std_logic                     := 'X';             -- chipselect
+			write_n    : in  std_logic                     := 'X';             -- write_n
+			writedata  : in  std_logic_vector(31 downto 0) := (others => 'X'); -- writedata
+			readdata   : out std_logic_vector(31 downto 0);                    -- readdata
+			address    : in  std_logic_vector(1 downto 0)  := (others => 'X'); -- address
+			reset_n    : in  std_logic                     := 'X';             -- reset_n
+			FREQ_IN    : in  std_logic                     := 'X'              -- export
+		);
+	end component avalon_anemometre;
+
+begin
+
+	avalon_anemometre_0 : component avalon_anemometre
+		port map (
+			clk        => clk,        --          clock.clk
+			chipselect => chipselect, -- avalon_slave_0.chipselect
+			write_n    => write_n,    --               .write_n
+			writedata  => writedata,  --               .writedata
+			readdata   => readdata,   --               .readdata
+			address    => address,    --               .address
+			reset_n    => reset_n,    --          reset.reset_n
+			FREQ_IN    => FREQ_IN     --    conduit_end.export
+		);
+
+end architecture rtl; -- of avalon_anemometre_0
